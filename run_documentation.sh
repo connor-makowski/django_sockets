1a5c4ce9aed03c3d756a34b62f37081ec4d6de5a
Connor Makowski <connorman528@gmail.com>
2025-01-29 14:05:17 -0500

    Update Docs.

--- run_documentation.sh
+++ run_documentation.sh
@@ -13,7 +13,7 @@ pip install -r requirements.txt > /dev/null
 printf "done.\n"
 
 # Specify versions for documentation purposes
-VERSION="2.0.0b7"
+VERSION="2.0.0"
 OLD_DOC_VERSIONS="1.2.0 1.1.0 1.0.0"
 export version_options="$VERSION $OLD_DOC_VERSIONS"
 
