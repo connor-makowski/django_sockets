ed8df2675a692966fb61edb99475dc65b91c8bb8
Connor Makowski <connorman528@gmail.com>
2026-07-20 14:51:25 -0400

    3.0.0: Bump version

--- utils/docs.py
+++ utils/docs.py
@@ -7,7 +7,7 @@ from pathlib import Path
 root = Path(__file__).parent.parent
 django_sockets_init = root / "django_sockets" / "__init__.py"
 
-VERSION = "3.0.0b1"
+VERSION = "3.0.0"
 OLD_DOC_VERSIONS = ["2.0.1", "1.2.0", "1.1.0", "1.0.0"]
 
 env = {
